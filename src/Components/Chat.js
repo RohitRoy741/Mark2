9cd5a95c21477222cd085f81a301185a5f5d86fb
Rohit Roy <rohit.roy@dell.com>
2022-03-21 12:52:22 +0530

    Plug Login to backend

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -1,5 +1,26 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 const Chat = (props) => {
-  return <h1>Hello World!</h1>;
+  const navigate = useNavigate();
+  const logoutHandler = () => {
+    localStorage.removeItem("token");
+    navigate("/");
+  };
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      navigate("/");
+    }
+  });
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <button type="submit" onClick={logoutHandler}>
+        Logout
+      </button>
+    </div>
+  );
 };
 
 export default Chat;
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,26 +1,52 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Login.css";
 
 const Login = (props) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const navigate = useNavigate();
   const usernameChangeHandler = (event) => {
     setUsername(event.target.value);
+    setError(false);
   };
   const passwordChangeHandler = (event) => {
     setPassword(event.target.value);
   };
   const submitHandler = (event) => {
     event.preventDefault();
+    setLoading(true);
     setUsername("");
     setPassword("");
     console.log(username, password);
+    fetch("http://127.0.0.1:3001/api/v1/users/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        setLoading(false);
+        if (result.status === "Success") {
+          console.log(result);
+          localStorage.setItem("token", result.data.token);
+          navigate("/chat");
+        } else {
+          setError(true);
+        }
+      });
   };
   return (
     <div className="login-page">
       <div className="login-card">
         <h1 className="login-heading">Mark 2</h1>
         <h2>Sign In!</h2>
+        {error ? "Authentication Failed" : null}
         <form className="login-form" onSubmit={submitHandler}>
           <input
             type="text"
@@ -36,7 +62,7 @@ const Login = (props) => {
             value={password}
             onChange={passwordChangeHandler}
           />
-          <button type="submit">Login</button>
+          <button type="submit">{loading ? "Loading..." : "Login"}</button>
           <p onClick={() => props.onPageChange("register")}>
             Don't have an account? Sign Up!
           </p>
